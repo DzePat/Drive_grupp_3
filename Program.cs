a354aeb2d2971aed8301e434d612b257eae42c7a
Dzedas <ddzedas@gmail.com>
2023-04-17 11:49:30 +0200

    Lagt till SkapaBana metod som skapar en bana

--- Program.cs
+++ Program.cs
@@ -1,10 +1,103 @@
-﻿namespace Drive
+﻿using System.ComponentModel;
+
+namespace Drive
 {
-    internal class Program
+    public class Program
     {
+        private static Random random = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (random)
+            {
+                return random.Next(min, max);
+            }
+        }
+        public static List<string> banan = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SkapaBana();
+            foreach(string c in banan)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        public static void SkapaBana()
+        {
+            int startpoint = GetRandomNumber(20, 60);
+            int previous = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (i == 0)
+                {
+                    string first = "";
+                    for (int j = 0; j < 80; j++)
+                    {
+                        if (startpoint == j)
+                        {
+                            first = first + "     ";
+                            j = j + 4;
+                        }
+                        else
+                        {
+                            first = first + 0;
+                        }
+                    }
+                    banan.Add(first);
+                    Console.WriteLine(banan[0]);
+                }
+                else if(i != 0)
+                {
+                    int addorsub;
+                    addorsub = GetRandomNumber(-1,1);
+                    int index = 0;
+                    foreach (char c in banan[previous])
+                    {
+                        if (c == ' ')
+                        {
+                            break;
+                        }
+                        else { index++; }
+                    }
+                    if (addorsub == -1)
+                    {
+                        string current = banan[previous];
+                        char[] chars = current.ToCharArray();
+                        if (index - 1 >= 0 && index + 4 <= 20) 
+                        {
+                            chars[index - 1] = ' ';
+                            chars[index + 4] = '0';
+                            string result = "";
+                            foreach (char c in chars)
+                            {
+                                result += c;
+                            }
+                            banan.Add(result);
+                        }
+                        else { banan.Add(banan[previous]); }
+                        
+                    }
+                    else if(addorsub == 1)
+                    {
+                        string current = banan[i - 1];
+                        char[] chars = current.ToCharArray();
+                        if(index+5 <= 20) {
+                            chars[index] = '0';
+                            chars[index + 5] = ' ';
+                            string result = "";
+                            foreach (char c in chars)
+                            {
+                                result += c;
+                            }
+                            banan.Add(result);
+                        }
+                        else { banan.Add(banan[previous]); }
+                        
+                    }
+                    else { banan.Add(banan[previous]); }
+                    previous = i;
+                }
+            }
         }
     }
 }

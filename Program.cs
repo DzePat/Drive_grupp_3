e7bb52bfb5616ab43ff804adf0c8a713ca3a8cbc
Dzedas <ddzedas@gmail.com>
2023-04-17 12:00:45 +0200

    Fixat filen efter att den har mergat fel

--- Program.cs
+++ Program.cs
@@ -19,9 +19,8 @@ namespace Drive
         public static List<string> banan = new List<string>();
         static void Main(string[] args)
         {
-
             SkapaBana();
-            foreach(string c in banan)
+            foreach (string c in banan)
             {
                 Console.WriteLine(c);
             }
@@ -51,10 +50,10 @@ namespace Drive
                     banan.Add(first);
                     Console.WriteLine(banan[0]);
                 }
-                else if(i != 0)
+                else if (i != 0)
                 {
                     int addorsub;
-                    addorsub = GetRandomNumber(-1,1);
+                    addorsub = GetRandomNumber(-1, 1);
                     int index = 0;
                     foreach (char c in banan[previous])
                     {
@@ -68,7 +67,7 @@ namespace Drive
                     {
                         string current = banan[previous];
                         char[] chars = current.ToCharArray();
-                        if (index - 1 >= 0 && index + 4 <= 20) 
+                        if (index - 1 >= 0 && index + 4 <= 20)
                         {
                             chars[index - 1] = ' ';
                             chars[index + 4] = '0';
@@ -80,13 +79,13 @@ namespace Drive
                             banan.Add(result);
                         }
                         else { banan.Add(banan[previous]); }
-                        
+
                     }
-                    else if(addorsub == 1)
+                    else if (addorsub == 1)
                     {
                         string current = banan[i - 1];
                         char[] chars = current.ToCharArray();
-                        if(index+5 <= 20) {
+                        if (index + 5 <= 20) {
                             chars[index] = '0';
                             chars[index + 5] = ' ';
                             string result = "";
@@ -97,20 +96,22 @@ namespace Drive
                             banan.Add(result);
                         }
                         else { banan.Add(banan[previous]); }
-                        
+
                     }
                     else { banan.Add(banan[previous]); }
                     previous = i;
                 }
             }
-
+        }
+        public static void menu()
+        {
             Console.WriteLine("This is a car game.");
             Console.WriteLine("Stay on the road!");
             Console.WriteLine("Use left, right and up arrows to control your car.");
             //Console.WriteLine($"Try to beat the highscore {XX} points"); //NYI
             do
             {
-              Console.Write("\nPress enter to start: ");
+                Console.Write("\nPress enter to start: ");
             } while (Console.ReadKey().Key != ConsoleKey.Enter);
 
 
@@ -118,13 +119,14 @@ namespace Drive
             {
                 //NYI - show game start on console
                 ConsoleKey key = Console.ReadKey().Key;
-                if (key == ConsoleKey.LeftArrow) {  }
-                else if (key == ConsoleKey.RightArrow) {  }
-                else if (key == ConsoleKey.UpArrow) {  }
+                if (key == ConsoleKey.LeftArrow) { }
+                else if (key == ConsoleKey.RightArrow) { }
+                else if (key == ConsoleKey.UpArrow) { }
                 else if (key == ConsoleKey.Escape || key == ConsoleKey.Q || key == ConsoleKey.X)
-                {Console.WriteLine("\nGame is interrupted"); break; }
-                else Console.WriteLine("wrong input"); 
+                { Console.WriteLine("\nGame is interrupted"); break; }
+                else Console.WriteLine("wrong input");
             } while (true);
+
 
 
         }

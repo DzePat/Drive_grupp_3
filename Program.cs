2cf8051a4e7afa40a4874f8d92be33d26fbdf0b4
Veryan <veryanray@hotmail.com>
2023-04-18 03:04:10 +0200

    Game over function, time function, started with bana generation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
-﻿using System;
+using System;
 using System.Globalization;
 using System.Text;
 
@@ -9,12 +9,11 @@ using System.Data;
 
 using System.Runtime.InteropServices;
 
-
 namespace Drive
 {
     public class Program
     {
-       
+
         private static Random random = new Random();
         public static int GetRandomNumber(int min, int max)
         {
@@ -28,49 +27,89 @@ namespace Drive
         public static List<string> banan = new List<string>();
         static void Main(string[] args)
         {
-            ConsoleKeyInfo input;
-            Console.SetWindowSize(82, 30);
-            SkapaBana();
-            string temp = "";
-            string bana = "";
-            int pos = getposition();
-
-            foreach (string c in banan)
-            {
-                bana = temp + c;
-                temp = $"{bana}\n";
-            }
-            do
-            {
-                if (Console.KeyAvailable == true)
-                {
-                    input = Console.ReadKey();
-                    if (input.Key == ConsoleKey.LeftArrow) --pos;
-                    else if (input.Key == ConsoleKey.RightArrow) ++pos;
-                };
-                StringBuilder sb = new StringBuilder(bana);
-                sb.Remove(pos, 1);
-                sb.Insert(pos, "A");
-                Console.WriteLine(sb.ToString());
-                Thread.Sleep(33);
-                Console.SetCursorPosition(0, 0);
-                Console.CursorVisible = false;
-            } while (true);
-        }
- 
-
-        public static void SkapaBana()
-        {
+            int points = 0;
+            bool gameIsPlayed = false;
             int previous = 0;
-            int addorsub;
-            Random rnd = new Random();
-            for (int i = 0; i < 20; i++)
+            ConsoleKeyInfo input;
+
+
+            Menu();
+            if (gameIsPlayed)
             {
-                if (i == 0)
+                Initialize();
+                SkapaBana();
+                string temp = "";
+                string bana = "";
+                int pos = getposition();
+
+                foreach (string c in banan)
+                {
+                    bana = temp + c;
+                    temp = $"{bana}\n";
+                }
+                do
+                {
+                    if (Console.KeyAvailable == true)
+                    {
+                        input = Console.ReadKey();
+                        if (input.Key == ConsoleKey.LeftArrow) --pos;
+                        else if (input.Key == ConsoleKey.RightArrow) ++pos;
+                    };
+                    StringBuilder sb = new StringBuilder(bana);
+                    if (bana[pos] == ' ')
+                    {
+                        sb.Remove(pos, 1);
+                        sb.Insert(pos, "A");
+                    }
+                    else
+                    {
+                        sb.Remove(pos, 1);
+                        sb.Insert(pos, "X");
+                        GameOver();
+                    }
+                    Console.WriteLine(sb.ToString());
+                    Thread.Sleep(33);
+                    Console.SetCursorPosition(0, 0);
+                    Console.CursorVisible = false;
+                } while (true);
+            }
+
+
+            static void Initialize()
+            {
+                int width = 82;
+                int height = 30;
+                int windowWidth, windowHeight;
+
+
+                if (OperatingSystem.IsWindows())
+                {
+                    Console.SetWindowSize(width, height);
+                }
+            }
+
+            void SkapaBana()
+            {
+                previous = 0;
+                for (int i = 0; i < 20; i++)
+                {
+                    CreateNewRow(previous);
+                    previous = i + 1;
+                }
+                
+            }
+
+            static string CreateNewRow(int previous)
+            {
+
+                int addorsub;
+                Random rnd = new Random();
+
+                if (previous == 0)
                 {
                     string first = "";
                     int j = 0;
-                    while(j < 80)
+                    while (j < 80)
                     {
                         if (startpoint == j)
                         {
@@ -85,11 +124,11 @@ namespace Drive
                     }
                     banan.Add(first);
                 }
-                else if (i != 0)
+                else
                 {
                     addorsub = rnd.Next(-1, 2);
                     int index = 0;
-                    foreach(char c in banan[previous])
+                    foreach (char c in banan[previous])
                     {
                         if (c == ' ')
                         {
@@ -108,126 +147,213 @@ namespace Drive
                     }
                     else { banan.Add(banan[previous]); }
                 }
-                previous = i;
-            }
-        }
 
-        public static int getposition()
-        {
-            int lastrow = banan.Count;
-            int pos = 0;
-            string bana = "";
-            string temp = "";
-            foreach (string c in banan)
-            {
-                bana = temp + c;
-                temp = $"{bana}\n";
-            }
-            for(int i = 1590;i < 1639; i++)
-            {
-                if (bana[i] == ' ')
-                {
-                    pos = i + 5;
-                    break;
+                    return banan[previous].ToString();
                 }
-            }
-            return pos;
-        }
 
-        private static string emptySpaces(int x)
-        {
-            string empty = "";
-            for(int i = 0;i < x; i++)
-            {
-                empty = empty + " ";
-            }
-            return empty;
-        }
-        public static string movestringtoright(string main)
-        {
-            char[] chars = main.ToCharArray();
-            char last = chars[chars.Length - 1];
-            char current;
-            char next = 'a';
-            for (int i = 0; i < main.Length-1;i++)
-            {       
-                if (i == 0)
+                static int getposition()
                 {
-                    current = chars[chars.Length-1];
-                    next = chars[i];
-                    chars[i] = current;
+                    int lastrow = banan.Count;
+                    int pos = 0;
+                    string bana = "";
+                    string temp = "";
+                    foreach (string c in banan)
+                    {
+                        bana = temp + c;
+                        temp = $"{bana}\n";
+                    }
+                    for (int i = 1590; i < 1639; i++)
+                    {
+                        if (bana[i] == ' ')
+                        {
+                            pos = i + 5;
+                            break;
+                        }
+                    }
+                    return pos;
                 }
-                else if(i > 0)
+
+                static string emptySpaces(int x)
                 {
-                    current = chars[i];
-                    chars[i] = next;
-                    next = current;
+                    string empty = "";
+                    for (int i = 0; i < x; i++)
+                    {
+                        empty = empty + " ";
+                    }
+                    return empty;
                 }
-            }
-            return toString(chars);
-        }
 
-        public static string movestringtoright2(string main)
-        {
-           string tomove = ""; 
-            for (int i = 0; i < main.Length; i++)
-            {  
-                if (i == 0) tomove += main[main.Length - 1];
-                else tomove += main[i-1];
-            }
-            return tomove;
-        }
-
-
-        public static string movestringtoleft(string main)
-        {
-            string tomove = "";
-            char first = main[0];
-            for(int i = 0; i < main.Length; i++)
-            {
-                if (i < main.Length - 1)
+                static string movestringtoright(string main)
                 {
-                    tomove = tomove + main[i + 1];
-                }              
+                    char[] chars = main.ToCharArray();
+                    char last = chars[chars.Length - 1];
+                    char current;
+                    char next = 'a';
+                    for (int i = 0; i < main.Length - 1; i++)
+                    {
+                        if (i == 0)
+                        {
+                            current = chars[chars.Length - 1];
+                            next = chars[i];
+                            chars[i] = current;
+                        }
+                        else if (i > 0)
+                        {
+                            current = chars[i];
+                            chars[i] = next;
+                            next = current;
+                        }
+                    }
+                    return toString(chars);
+                }
+
+                static string movestringtoright2(string main)
+                {
+                    string tomove = "";
+                    for (int i = 0; i < main.Length; i++)
+                    {
+                        if (i == 0) tomove += main[main.Length - 1];
+                        else tomove += main[i - 1];
+                    }
+                    return tomove;
+                }
+
+                static string movestringtoleft(string main)
+                {
+                    string tomove = "";
+                    char first = main[0];
+                    for (int i = 0; i < main.Length; i++)
+                    {
+                        if (i < main.Length - 1)
+                        {
+                            tomove = tomove + main[i + 1];
+                        }
+                    }
+                    tomove = tomove + first;
+                    return tomove;
+                }
+
+                static string toString(char[] chars)
+                {
+                    string thestring = "";
+                    foreach (char c in chars)
+                    {
+                        thestring = thestring + c;
+                    }
+                    return thestring;
+                }
+
+                void Menu()
+                {
+                    Console.WriteLine("This is a car game.");
+                    Console.WriteLine("Stay on the road!");
+                    Console.WriteLine("Use left, right and up arrows to control your car.");
+                    //Console.WriteLine($"Try to beat the highscore {XX} points"); //NYI
+                    Console.WriteLine("Press enter to start: ");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+                    {
+                        ConsoleKey key = Console.ReadKey().Key;
+                        if (key == ConsoleKey.Enter)
+                        {//FIXME buggar ibland när man startar? vet inte varför
+                            Console.Clear();
+                            gameIsPlayed = true;
+                        }
+                    }
+
+
+                    /* do
+                     {
+                         //NYI - show game start on console
+                         ConsoleKey key = Console.ReadKey().Key;
+                         if (key == ConsoleKey.LeftArrow) { }
+                         else if (key == ConsoleKey.RightArrow) { }
+                         else if (key == ConsoleKey.UpArrow) { }
+                         else if (key == ConsoleKey.Escape || key == ConsoleKey.Q || key == ConsoleKey.X)
+                         { Console.WriteLine("\nGame is interrupted"); break; }
+                         else Console.WriteLine("wrong input");
+                     } while (true);
+                    */
+
+
+
+                }
+
+                void TimerPoints()
+                {
+                    int seconds = 0;
+                    int minutes = 0;
+
+                    int pointsMultiplier = 10;
+
+                    while (gameIsPlayed)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(1000));
+                        seconds++;
+                        points = (seconds + (minutes * 60)) * pointsMultiplier;
+
+                        if (seconds == 60)
+                        {
+                            seconds = 0;
+                            minutes++;
+                        }
+                        Console.Write(seconds.ToString("D2") + ":" + minutes.ToString("D2").PadRight(10) + "Points: " + points);
+                    }
+                }
+
+                void Update()
+                {
+                    int lastRow = banan.Count() - 1;
+                    banan.RemoveAt(lastRow);
+                
+                    for (int i = lastRow - 1; i > 0; i--)
+                    {
+                    
+                    var item = banan[i];
+                        banan.RemoveAt(i);
+                        banan.Insert(i + 1, item);
+                    }
+                     var newItem = CreateNewRow(previous);
+                    banan.Insert(0, newItem);
+
+
+                }
+
+                void GameOver()
+                {
+                    gameIsPlayed = false;
+                    Console.Clear();
+                    Console.WriteLine("GAME OVER!");
+                    Thread.Sleep(TimeSpan.FromMilliseconds(1000));
+                    Console.WriteLine($"Your score: {points}");
+                    while (true)
+                    {
+                        Console.WriteLine("Play Again?");
+                        Console.WriteLine("'Y' / 'N'");
+                        char answer = Console.ReadKey().KeyChar;
+                        if (answer == 'Y' || answer == 'y')
+                        {
+                            Console.Clear();
+                            Menu();
+                            return;
+                        }
+                        else if (answer == 'N' || answer == 'n')
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Thanks for playing");
+                            Thread.Sleep(TimeSpan.FromMilliseconds(1000));
+                            Environment.Exit(0);
+                            return;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Invalid answer, please answer 'Y' for yes or 'N' for no");
+                        }
+                    }
+
+                }
+
+
             }
-            tomove = tomove + first;
-            return tomove;
-        }
-        public static string toString(char[] chars)
-        {
-            string thestring = "";
-            foreach(char c in chars)
-            {
-                thestring = thestring + c;
-            }
-            return thestring;
-        }
-        public static void menu()
-        {
-            Console.WriteLine("This is a car game.");
-            Console.WriteLine("Stay on the road!");
-            Console.WriteLine("Use left, right and up arrows to control your car.");
-            //Console.WriteLine($"Try to beat the highscore {XX} points"); //NYI
-            do
-            {
-                Console.Write("\nPress enter to start: ");
-            } while (Console.ReadKey().Key != ConsoleKey.Enter);
-
-
-            do
-            {
-                //NYI - show game start on console
-                ConsoleKey key = Console.ReadKey().Key;
-                if (key == ConsoleKey.LeftArrow) { }
-                else if (key == ConsoleKey.RightArrow) { }
-                else if (key == ConsoleKey.UpArrow) { }
-                else if (key == ConsoleKey.Escape || key == ConsoleKey.Q || key == ConsoleKey.X)
-                { Console.WriteLine("\nGame is interrupted"); break; }
-                else Console.WriteLine("wrong input");
-            } while (true);
-
-
-
         }
     }
-}
